a6620306ac806b1afa1f1e638226f0b354d5c392
Suslik <123ivanovivanivan@gmail.com>
2025-04-19 17:59:33 +0700

    0.1
    Add

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            PlayButtonMain = new RoundButton();
+            SudokuMainLabel = new RoundedLabel();
+            SuspendLayout();
+            // 
+            // PlayButtonMain
+            // 
+            PlayButtonMain.Anchor = AnchorStyles.None;
+            PlayButtonMain.BackColor = Color.Gray;
+            PlayButtonMain.Font = new Font("Showcard Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PlayButtonMain.ForeColor = Color.Plum;
+            PlayButtonMain.Image = (Image)resources.GetObject("PlayButtonMain.Image");
+            PlayButtonMain.Location = new Point(121, 288);
+            PlayButtonMain.Name = "PlayButtonMain";
+            PlayButtonMain.Size = new Size(278, 80);
+            PlayButtonMain.TabIndex = 0;
+            PlayButtonMain.Text = "PLAY";
+            PlayButtonMain.TextImageRelation = TextImageRelation.TextBeforeImage;
+            PlayButtonMain.UseVisualStyleBackColor = false;
+            PlayButtonMain.Click += StartGame;
+            // 
+            // SudokuMainLabel
+            // 
+            SudokuMainLabel.Anchor = AnchorStyles.None;
+            SudokuMainLabel.BackColor = Color.Gray;
+            SudokuMainLabel.Font = new Font("Showcard Gothic", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SudokuMainLabel.ForeColor = Color.Plum;
+            SudokuMainLabel.Location = new Point(121, 84);
+            SudokuMainLabel.Name = "SudokuMainLabel";
+            SudokuMainLabel.Size = new Size(278, 74);
+            SudokuMainLabel.TabIndex = 1;
+            SudokuMainLabel.Text = "SUDOKU";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(534, 421);
+            Controls.Add(SudokuMainLabel);
+            Controls.Add(PlayButtonMain);
+            ForeColor = Color.FromArgb(224, 224, 224);
+            Name = "Form1";
+            Padding = new Padding(50);
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RoundButton PlayButtonMain;
+        private RoundedLabel SudokuMainLabel;
+    }
+}
---
+++ SudokuForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Sudoku
+{
+    public partial class SudokuForm
+    {
+        private RoundButton ResetButton;
+        private RoundButton DrawButton;
+        private RoundButton CheckButton;
+
+        private void InitializeComponent()
+        {
+            ResetButton = new RoundButton();
+            DrawButton = new RoundButton();
+            CheckButton = new RoundButton();
+            SuspendLayout();
+            // 
+            // ResetButton
+            // 
+            ResetButton.BackColor = Color.Gray;
+            ResetButton.Location = new Point(80, 506);
+            ResetButton.Name = "ResetButton";
+            ResetButton.Size = new Size(69, 60);
+            ResetButton.TabIndex = 0;
+            ResetButton.Text = "roundButton1";
+            ResetButton.UseVisualStyleBackColor = false;
+            ResetButton.Click += ResetButton_Click;
+            // 
+            // DrawButton
+            // 
+            DrawButton.BackColor = Color.Gray;
+            DrawButton.Font = new Font("Showcard Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            DrawButton.ForeColor = Color.Plum;
+            DrawButton.Location = new Point(168, 506);
+            DrawButton.Name = "DrawButton";
+            DrawButton.Size = new Size(156, 64);
+            DrawButton.TabIndex = 1;
+            DrawButton.Text = "Draw";
+            DrawButton.UseVisualStyleBackColor = false;
+            // 
+            // CheckButton
+            // 
+            CheckButton.BackColor = Color.Gray;
+            CheckButton.Location = new Point(343, 506);
+            CheckButton.Name = "CheckButton";
+            CheckButton.Size = new Size(69, 60);
+            CheckButton.TabIndex = 2;
+            CheckButton.Text = "roundButton3";
+            CheckButton.UseVisualStyleBackColor = false;
+            CheckButton.Click += CheckButton_Click;
+            // 
+            // SudokuForm
+            // 
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(515, 593);
+            Controls.Add(CheckButton);
+            Controls.Add(DrawButton);
+            Controls.Add(ResetButton);
+            MaximizeBox = false;
+            Name = "SudokuForm";
+            Text = "Sudoku";
+            ResumeLayout(false);
+        }
+    }
+}
